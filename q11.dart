3cbddbba991345c9a24a2e0e4095cc6a4d9014be
alkofahijo <alkofahijo95@gmail.com>
2025-07-31 22:08:09 +0300

    Update q11.dart

--- q11.dart
+++ q11.dart
@@ -17,7 +17,7 @@ void main() {
   while (number != 0) {
     int digit = number % 10;
     reversed = reversed * 10 + digit;
-    number ~/= 10; // Integer division
+    number ~/= 10;
   }
 
   if (originalNumber == reversed) {
